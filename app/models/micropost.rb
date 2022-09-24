a634644bd5c5254d606253bf4a809ce345763abf
Hironobu-okubo <ee171068oh@gmail.com>
2022-09-25 01:34:41 +0900

    for heroku

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-   validates :content, presence: true, length: { maximum: 255 }
+  validates :content, presence: true, length: { maximum: 255 }
+   
+  has_many :likerelationships
+  has_many :liked, through: :likerelationships, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
   has_many :microposts
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
+  has_many :likerelationships
+  has_many :like, through: :likerelationships, source: :micropost
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
